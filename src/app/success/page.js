30a6be7d5a218e369bee51e1ffe6a184d7353327
Abhishek Tomar <itsabhi.at@gmail.com>
2024-05-30 14:30:50 +0530

    minor change

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -3,16 +3,42 @@
 import { useEffect } from "react";
 
 const Success = () => {
-  //   useEffect(() => {
-  //     const hasRefreshed = localStorage.getItem("hasRefreshed");
+  useEffect(() => {
+    const sendDataToBackend = async (data) => {
+      try {
+        const response = await axios.post("/api/payment/success", data);
 
-  //     if (!hasRefreshed) {
-  //       localStorage.setItem("hasRefreshed", "true");
-  //       window.location.reload();
-  //     } else {
-  //       localStorage.removeItem("hasRefreshed");
-  //     }
-  //   }, []);
+        if (response.status === 200) {
+          console.log("Data sent successfully:", response.data);
+        } else {
+          console.error("Failed to send data:", response.data);
+        }
+      } catch (error) {
+        console.error("Error sending data:", error);
+      }
+    };
+
+    const handleFormData = () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const encResp = urlParams.get("encResp");
+      const orderNo = urlParams.get("orderNo");
+      const accessCode = urlParams.get("accessCode");
+      //   const crossSellUrl = urlParams.get("crossSellUrl");
+
+      if (encResp && orderNo && accessCode) {
+        const formData = {
+          encResp,
+          orderNo,
+          accessCode,
+          //   crossSellUrl,
+        };
+
+        sendDataToBackend(formData);
+      }
+    };
+
+    handleFormData();
+  }, []);
 
   return (
     <div className="flex items-center justify-center min-h-screen w-full text-white text-2xl relative z-30">
