4aed8ffcf7ad875bc7adb7500cd36024c9766d1d
Abhishek Tomar <itsabhi.at@gmail.com>
2024-05-31 11:56:31 +0530

    minor change

--- src/app/failure/page.js
+++ src/app/failure/page.js
@@ -1,23 +1,40 @@
-// pages/failure.js
+// pages/success.js
 "use client";
 import { useEffect } from "react";
-
+import failed from "@/app/assets/failed.svg";
+import Image from "next/image";
 const Failure = () => {
   useEffect(() => {
-    const hasRefreshed = localStorage.getItem("hasRefreshed");
+    if (typeof window !== "undefined") {
+      const hasRefreshed = sessionStorage.getItem("hasRefreshedOnce");
 
-    if (!hasRefreshed) {
-      localStorage.setItem("hasRefreshed", "true");
-      window.location.reload();
-    } else {
-      localStorage.removeItem("hasRefreshed");
+      if (!hasRefreshed) {
+        sessionStorage.setItem("hasRefreshedOnce", "true");
+        window.location.reload();
+      }
     }
   }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen w-full text-white text-2xl relative z-30">
-      Payment Failed. Please try again.
-    </div>
+    <main className="md:h-screen h-[90vh] min-h-screen relative z-30">
+      <div className="h-full w-full flex items-center justify-center md:pt-0 pt-6">
+        <div className="bg-black bg-opacity-70 md:h-[70%] md:w-[70%] w-[80%] h-[80vh] overflow-auto rounded-lg p-4 text-white">
+          <Image
+            src={failed}
+            alt=""
+            height={200}
+            width={200}
+            className="mx-auto"
+          />
+          <p className="text-2xl mx-auto w-full text-center mt-8">
+            Your payment is Failed.
+          </p>
+          <p className="mx-auto px-0 md:px-12 w-full text-center mt-4">
+            Oops! Something Went Wrong
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,23 +1,42 @@
 // pages/success.js
 "use client";
 import { useEffect } from "react";
-
+import success from "@/app/assets/success.svg";
+import Image from "next/image";
 const Success = () => {
   useEffect(() => {
-    const hasRefreshed = localStorage.getItem("hasRefreshed");
+    if (typeof window !== "undefined") {
+      const hasRefreshed = sessionStorage.getItem("hasRefreshedOnce");
 
-    if (!hasRefreshed) {
-      localStorage.setItem("hasRefreshed", "true");
-      window.location.reload();
-    } else {
-      localStorage.removeItem("hasRefreshed");
+      if (!hasRefreshed) {
+        sessionStorage.setItem("hasRefreshedOnce", "true");
+        window.location.reload();
+      }
     }
   }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen w-full text-white text-2xl relative z-30">
-      Payment Successful!
-    </div>
+    <main className="md:h-screen h-[90vh] min-h-screen relative z-30">
+      <div className="h-full w-full flex items-center justify-center md:pt-0 pt-6">
+        <div className="bg-black bg-opacity-70 md:h-[70%] md:w-[70%] w-[80%] h-[80vh] overflow-auto rounded-lg p-4 text-white">
+          <Image
+            src={success}
+            alt=""
+            height={200}
+            width={200}
+            className="mx-auto"
+          />
+          <p className="text-2xl mx-auto w-full text-center mt-8">
+            Your payment is successful.{" "}
+          </p>
+          <p className="mx-auto px-0 md:px-12 w-full text-center mt-4">
+            Thank you for registering for BDS 2024. You will receive
+            registration confirmation along with paid invoice on your registered
+            email id.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 
